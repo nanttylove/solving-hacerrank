3adad52cf393b555cc29c79c41469431034236a0
Pranaya Yodsuwan <nanttylove@gmail.com>
2021-09-20 21:10:42 +0700

    Create solveMyFirst.js

---
+++ solveMyFirst.js
@@ -0,0 +1,11 @@
+//Solve Me First
+function solveMeFirst(a, b) {
+  //โจทย์ให้ทำฟังก์ชัน solveMefirst รับค่า 2 ตัวคือ a และ b
+  sum = a + b; // สร้างตัวแปรชื่อ sum มีค่าเท่ากับ a+b และให้
+  return sum; // ให้ return sum หรือ a+b นั่นละออกมา
+}
+console.log(solveMeFirst(1, 2)); // ให้ปริ้นฟังก์ชัน solveMeFirst ซึ่ง รับค่า a=1 และ b=2
+const c = solveMeFirst(1, 2); // ลองอีกแบบคือ ประกาศตัวแปร c มีค่าเท่ากับ การกระทำจาก solveMeFirst
+console.log(c); // ปริ้น c
+
+// ผลการรันข้อนี้คือ 3 และ 3
